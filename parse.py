4cdbaee5036015ba961d8ea31a1a96d912780460
The Grinch <grinch@Grinchbook.local>
2010-11-30 16:22:11 -0500

    Got first piece of WM syntax working (recursive, too!): newlines.

--- parse.py
+++ parse.py
@@ -10,8 +10,26 @@ italic = Literal("''")
 char = oneOf(list(string.printable))
 # TODO: Optimize by turning into something like...
 # text = Word(string.printable)
-inline = bold | italic | char  # in order of precedence
+inline = bold | italic | char  # in order of precedence: see docs at MatchFirst
 stuff = ZeroOrMore(inline)
 
-p = stuff.parseString("'''boo hoohoo''")
-print p
+nice_char = oneOf(['a', 'b', 'c'])
+bingy = Group(Literal('!') + Combine(OneOrMore(nice_char)) + Literal('@'))
+stuff = OneOrMore(bingy)
+
+# Real MediaWiki syntax:
+newline = (Literal('\r\n') | Literal('\n\r') | Literal('\r') | Literal('\n')).leaveWhitespace()
+newlines_f = Forward()
+newlines = (newline + Optional(newlines_f)).leaveWhitespace()
+newlines_f << newlines
+
+newlines.verbose_stacktrace = True
+str = "\r\n\r\n\n"
+print repr(str)
+try:
+    p = newlines.parseString(str)
+except ParseException, e:
+    print repr(e.msg)
+    raise
+else:
+    print p
