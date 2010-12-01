aed9020318ce8c2c913982d797aa19bbc1b1cfb6
The Grinch <grinch@Grinchbook.local>
2010-11-30 23:41:50 -0800

    Implement spaces, tabs, and other assorted whitespace.

--- parse.py
+++ parse.py
@@ -3,6 +3,13 @@
 from pyparsing import *
 import string
 
+
+def parsed_eq(expr, text, want):
+    got = list(expr.parseString(text))
+    if got != want:
+        raise AssertionError('%s != %s' % (got, want))
+
+
 ParserElement.enablePackrat()  # Enable memoizing.
 
 bold = Literal("'''")
@@ -18,18 +25,28 @@ bingy = Group(Literal('!') + Combine(OneOrMore(nice_char)) + Literal('@'))
 stuff = OneOrMore(bingy)
 
 # Real MediaWiki syntax:
+## Fundamental elements:
 newline = (Literal('\r\n') | Literal('\n\r') | Literal('\r') | Literal('\n')).leaveWhitespace()
-newlines_f = Forward()
-newlines = (newline + Optional(newlines_f)).leaveWhitespace()
-newlines_f << newlines
+newlines = OneOrMore(newline)
+#newlines.verbose_stacktrace = True
+parsed_eq(newlines, '\r\r\n\n\r\n', ['\r', '\r\n', '\n\r', '\n'])
 
-newlines.verbose_stacktrace = True
-str = "\r\n\r\n\n"
-print repr(str)
-try:
-    p = newlines.parseString(str)
-except ParseException, e:
-    print repr(e.msg)
-    raise
-else:
-    print p
+space = Literal(' ').leaveWhitespace()
+spaces = OneOrMore(space)
+parsed_eq(spaces, '  ', [' ', ' '])
+space_tab = (space | Literal('\t').leaveWhitespace()).leaveWhitespace().parseWithTabs()
+parsed_eq(space_tab, '\t', ['\t'])
+space_tabs = OneOrMore(space_tab)
+
+whitespace_char = (space_tab | newline).leaveWhitespace().parseWithTabs()
+parsed_eq(whitespace_char, '\t', ['\t'])
+whitespace = (OneOrMore(whitespace_char) | StringEnd()).leaveWhitespace().parseWithTabs()
+parsed_eq(whitespace, ' \t\r', [' ', '\t', '\r'])
+
+# try:
+#     p = newlines.parseString(str)
+# except ParseException, e:
+#     print repr(e.msg)
+#     raise
+# else:
+#     print p
