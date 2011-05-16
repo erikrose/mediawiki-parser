88f63edfbc9763e405ae51b2612efa12c15cb230
Peter Potrowl <peter017@gmail.com>
2011-05-17 00:48:01 +0200

    Continue tests on pijnu

--- pijnu/wikitext-test.py
+++ pijnu/wikitext-test.py
@@ -2,7 +2,6 @@
 from pijnu import makeParser
 mediawikiGrammar = file("mediawiki.pijnu").read()
 mediawikiParser = makeParser(mediawikiGrammar)
-print mediawikiParser
 
 # use it
 source = file("wikitext.txt").read()
