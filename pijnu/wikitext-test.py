75bb3b58a5fe648ed297cf40fba22c68f0a48178
Peter Potrowl <peter017@gmail.com>
2011-05-17 21:32:06 +0200

    pijnu moved to spikes/

--- pijnu/wikitext-test.py
+++
@@ -1,8 +0,0 @@
-# get the parser
-from pijnu import makeParser
-mediawikiGrammar = file("mediawiki.pijnu").read()
-mediawikiParser = makeParser(mediawikiGrammar)
-
-# use it
-source = file("wikitext.txt").read()
-mediawikiParser.test(source)
