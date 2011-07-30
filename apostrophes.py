f3868612b1b6658b590ae28b73255c8094c9bacf
Erik Rose <erik@mozilla.com>
2011-07-29 23:37:02 -0700

    Improve style and possibly speed of apostrophe parser.
    enumerate() instead of zip() will save copying all the split bits of the string once for each loop.

--- apostrophes.py
+++ apostrophes.py
@@ -15,28 +15,29 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
-"""
 
+"""
 import re
 
-_quotePat = re.compile(u"""(''+)""", re.UNICODE)
+_quotePat = re.compile(u"(''+)", re.UNICODE)
 
 default_tags = {'bold': '<strong>',
                 'bold_close': '</strong>',
                 'italic': '<em>',
                 'italic_close': '</em>'}
 
+
 def _parseQuotes(text, tags=default_tags):
         arr = _quotePat.split(text)
         if len(arr) == 1:
             return text
+
         # First, do some preliminary work. This may shift some apostrophes from
         # being mark-up to being text. It also counts the number of occurrences
         # of bold and italics mark-ups.
-        numBold = 0
-        numItalics = 0
-        for i,r in zip(range(len(arr)), arr):
-            if i%2 == 1:
+        numBold = numItalics = 0
+        for i, r in enumerate(arr):
+            if i % 2:
                 l = len(r)
                 if l == 4:
                     arr[i-1] += u"'"
@@ -56,12 +57,10 @@ def _parseQuotes(text, tags=default_tags):
         # that one of the bold ones was meant to be an apostrophe followed
         # by italics. Which one we cannot know for certain, but it is more
         # likely to be one that has a single-letter word before it.
-        if numBold%2 == 1 and numItalics%2 == 1:
-            firstSingleLetterWord = -1
-            firstMultiLetterWord = -1
-            firstSpace = -1
-            for i,r in zip(range(len(arr)), arr):
-                if i%2 == 1 and len(r) == 3:
+        if numBold % 2 and numItalics % 2:
+            firstSingleLetterWord = firstMultiLetterWord = firstSpace = -1
+            for i, r in enumerate(arr):
+                if i % 2 and len(r) == 3:
                     x1 = arr[i-1][-1:]
                     x2 = arr[i-1][-2:-1]
                     if x1 == u' ':
@@ -70,31 +69,30 @@ def _parseQuotes(text, tags=default_tags):
                     elif x2 == u' ':
                         if firstSingleLetterWord == -1:
                             firstSingleLetterWord = i
-                    else:
-                        if firstMultiLetterWord == -1:
-                            firstMultiLetterWord = i
+                    elif firstMultiLetterWord == -1:
+                        firstMultiLetterWord = i
 
             # If there is a single-letter word, use it!
             if firstSingleLetterWord > -1:
                 arr[firstSingleLetterWord] = u"''"
-                arr[firstSingleLetterWord-1] += u"'"
+                arr[firstSingleLetterWord - 1] += u"'"
             # If not, but there's a multi-letter word, use that one.
             elif firstMultiLetterWord > -1:
                 arr[firstMultiLetterWord] = u"''"
-                arr[firstMultiLetterWord-1] += u"'"
+                arr[firstMultiLetterWord - 1] += u"'"
             # ... otherwise use the first one that has neither.
             # (notice that it is possible for all three to be -1 if, for example,
             # there is only one pentuple-apostrophe in the line)
             elif firstSpace > -1:
                 arr[firstSpace] = u"''"
-                arr[firstSpace-1] += u"'"
+                arr[firstSpace - 1] += u"'"
 
         # Now let's actually convert our apostrophic mush to HTML!
         output = []
         buffer = []
         state = ''
-        for i,r in zip(range(len(arr)), arr):
-            if i%2 == 0:
+        for i, r in enumerate(arr):
+            if not i % 2:
                 if state == 'both':
                     buffer.append(r)
                 else:
@@ -169,6 +167,7 @@ def _parseQuotes(text, tags=default_tags):
             output.append(u''.join(buffer))
             output.append(tags['bold_close']+tags['italic_close'])
         return u''.join(output)
+
 
 def parseAllQuotes(text, tags=default_tags):
     sb = []
